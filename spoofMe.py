459b802a467c4f97098411e3f6b93159bcb2bf6b
x11x00x00x <134183148+x11x00x00x@users.noreply.github.com>
2024-06-10 08:59:48 -0500

    Updated spoofMe.py and added response folder.
    Updated spoofMe.py to work with the current version of SpoofCard. Added some error handling and removed some features that are no longer available.

--- spoofMe.py
+++ spoofMe.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python
-# coding: utf-8
-
-# Autor: Marcelo Vázquez (aka s4vitar)
-
 import re, shutil, tempfile, signal
-import requests
+from bs4 import BeautifulSoup
 import sys
 import ssl
 import os
+import requests
+import urllib3
+
+urllib3.disable_warnings(category = urllib3.exceptions.InsecureRequestWarning)
 
 from time import sleep
 
@@ -22,25 +21,25 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 if len(sys.argv) != 4:
-	print "\n" + bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.OKGREEN + "] Usage: python " + sys.argv[0] + " <area-code> <phone-number> <login-pin>\n" + bcolors.ENDC
-	print bcolors.BOLD + "Example: python " + sys.argv[0] + " 34 XXXXXXXXX 1234\n" + bcolors.ENDC
+	print("\n" + bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.OKGREEN + "] Usage: python " + sys.argv[0] + " <country-code> <phone-number> <login-pin>\n" + bcolors.ENDC)
+	print(bcolors.BOLD + "Example: python " + sys.argv[0] + " 1 XXXXXXXXX 1234\n" + bcolors.ENDC)
 	sys.exit(0)
 
 def signal_handler(key, frame):
-	print "\n\n[*] Exiting...\n"
+	print("\n\n[*] Exiting...\n")
 	sys.exit(1)
 
 signal = signal.signal(signal.SIGINT, signal_handler)
-
-area_code = sys.argv[1]
-phone_number = "+" + area_code + sys.argv[2]
+area_code = sys.argv[1].replace("+", "")
+phone_number = sys.argv[2]
 login_pin = sys.argv[3]
 
-url = 'https://www.spoofcard.com/login'
-url_account = 'https://www.spoofcard.com/account'
-url_settings = 'https://www.spoofcard.com/account/settings'
-url_call_spoof = 'https://www.spoofcard.com/account/calls/create'
-url_sms_spoof = 'https://www.spoofcard.com/account/two-way-sms/create'
+# URL of the login page
+url = 'https://web.spoofcard.com/login'
+url_account = 'https://web.spoofcard.com/account'
+url_settings = 'https://web.spoofcard.com/account/settings'
+url_call_spoof = 'https://web.spoofcard.com/account/calls/create'
+url_sms_spoof = 'https://web.spoofcard.com/account/two-way-sms/create'
 
 def sed_inplace(filename, pattern, repl):
 
@@ -55,59 +54,78 @@ def sed_inplace(filename, pattern, repl):
     shutil.move(tmp_file.name, filename)
 
 def sms_spoofing():
-	destination_number = raw_input('\nDestination Number [+34XXXXXXXXX]: ')
-	display_number = raw_input('\nDisplay Number [+34XXXXXXXXX]: ')
-	message = raw_input('\nEnter message to display: ')
+	destination_number = input('\nDestination Number [+1XXXXXXXXX]: ')
+	display_number = input('\nDisplay Number [+1XXXXXXXXX]: ')
+	message = input('\nEnter message to display: ')
 
 	sms_spoofing_data = {'body': message, 'destination_address': destination_number, 'source_address': display_number, 'tos_accepted': 'true', 'oneWay': 'true'}
 	sms_spoofing_headers = {'accept-encoding': 'gzip, deflate, br', 'accept-language': 'es-ES,es;q=0.9,en;q=0.8', 'content-type': 'application/x-www-form-urlencoded', 'accept': 'application/json, text/plain, */*', 'x-requested-with': 'XMLHttpRequest'}
 
 	r = session.post(url_sms_spoof, data=sms_spoofing_data, headers=sms_spoofing_headers, verify=False)
-
-	print "\n[*] Message has been sent\n"
+	
+	content_file = open("./response/sms_response.txt", "w")
+	content_file.write(r.content.decode())
+	content_file.close()
+	
+	with open('./response/sms_response.txt') as f:
+			for lines in f:
+				if (re.search("Unexpected", lines)):
+					print("\n" + bcolors.FAIL + "[" + bcolors.ENDC + bcolors.FAIL + "*" + bcolors.ENDC + bcolors.FAIL + "] Message has failed to send\n" + bcolors.ENDC)
+				else:
+					print("\n" + bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] Message has been sent\n" + bcolors.ENDC)
 
 def call_spoofing():
-	destination_number = raw_input('\nDestination Number [+34XXXXXXXXX]: ')
-	display_number = raw_input('\nDisplay Number [+34XXXXXXXXX]: ')
+	destination_number = input('\nDestination Number [+1XXXXXXXXX]: ')
+	display_number = input('\nDisplay Number [+1XXXXXXXXX]: ')
 
-	call_spoofing_data = {'destination_address': destination_number, 'display_address': display_number, 'event_type': 'outbound-call', 'plugins[background_noise][choice_index]': '9'}
+	call_spoofing_data = {'destination_address': destination_number, 'display_address': display_number, 'event_type': 'outbound-call', 'plugins[background_noise][choice_index]': '8'}
 	call_spoofing_headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'accept': 'application/json, text/javascript, */*; q=0.01', 'x-requested-with': 'XMLHttpRequest'}
 
 	r = session.post(url_call_spoof, data=call_spoofing_data, headers=call_spoofing_headers, verify=False)
 
-	content_file = open("dial_response.txt", "w")
-	content_file.write(r.content)
+	content_file = open("./response/dial_response.txt", "w")
+	content_file.write(r.content.decode())
 	content_file.close()
-
-	sed_inplace('dial_response.txt', r'\,', '\n')
+    
+	sed_inplace('./response/dial_response.txt', r'\,', '\n')
 
 	dial_access_code = []
 
-	with open('dial_response.txt') as f:
-	        for lines in f:
+	with open('./response/dial_response.txt') as f:
+			for lines in f:
 				if re.search("access_code", lines):
 					dial_access_code.append(lines.split(':')[1].split('"')[1])
 
-	with open('dial_numbers.txt') as f:
-		dial_numbers = [lines.rstrip('\n') for lines in f.readlines()]
+	dial_access_number = []
 
-	print '\n--------------------------------------------'
+	with open('./response/dial_response.txt') as f:
+			for lines in f:
+				if re.search("last_used_access_number", lines):
+					dial_access_number.append(lines.split(':')[1].split('"')[1])
 
-	for numbers in dial_numbers:
-		print numbers
+	print('\n--------------------------------------------')
 
-	print "\n------------------"
-	print "Access-Code: %s |" % dial_access_code[0]
-	print '--------------------------------------------\n'
+	# for numbers in dial_numbers:
+	# print(numbers)
 
-	print "[*] Now call any of the numbers listed above [It is recommended to call the number +39 0356306566]"
-	print "[*] Once done, you will need to enter the Access-Code provided above"
-	print "[*] When finished, press <Enter> key to finish the program\n"
+	print("\n------------------")
+	print("Access-Number: %s |" % dial_access_number[0])
+	print("Access-Code: %s |" % dial_access_code[0])
+	print('--------------------------------------------\n')
 
-	raw_input("Press <Enter> to continue...")
+	print("[*] Now call the number listed above.")
+	print("[*] Once done, you will need to enter the Access-Code provided above")
+	print("[*] When finished, press <Enter> key to finish the program\n")
+
+	input("Press <Enter> to continue...")
 
 	os.remove("dial_response.txt")
 	session.close()
+      
+
+
+
+
 
 banner = "\n╱╱╱╱╱╱╱╱╱╱╱╱╱╭━┳━╮╭━╮\n"
 banner += "╱╱╱╱╱╱╱╱╱╱╱╱╱┃╭┫┃╰╯┃┃\n"
@@ -118,81 +136,89 @@ banner += "╰━━┫╭━┻━━┻━━╯╰╯╰╯╰╯╰┻━━
 banner += "╱╱╱┃┃\n"
 banner += "╱╱╱╰╯\n"
 
-print banner
+print(banner)
 
-login = {'provider_type': 'phone', 'access_token': '', 'redirect_url': '', 'countrycode': area_code, 'phone_number': phone_number, 'login-pin': login_pin}
 
-requests.packages.urllib3.disable_warnings()
+print("\n" + bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] Collecting data...\n" + bcolors.ENDC)
 
-print "\n" + bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] Collecting data...\n" + bcolors.ENDC
+os.remove("./response/content_response.txt")
+
 
 session = requests.Session()
-r = session.post(url, data=login, verify=False)
 
-content_file = open("content_response.txt", "w")
-content_file.write(r.content)
-content_file.close()
+# Send a GET request to the login page to get the initial HTML
+response = session.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-with open('content_response.txt') as f:
-	for lines in f:
-		if re.search("incorrect", lines):
-			print bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] The data entered does not correspond to any account\n" + bcolors.ENDC
-			print bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] Please, create an account first at https://www.spoofcard.com\n" + bcolors.ENDC
-			os.remove("content_response.txt")
+# Find the login form
+form = soup.find('form', {'id': 'loginForm'})  # adjust the form ID if needed
+
+# Extract the form fields
+fields = {}
+for input_field in form.find_all('input'):
+    # fields[input_field['name']] = input_field.get('value', '')
+	fields['provider_type'] = 'phone'
+	fields['access_token'] = ''
+	fields['redirect_url'] = ''
+	fields['countrycode'] = str(area_code)
+	fields['phone_number'] = str(phone_number)
+	fields['login-pin'] = str(login_pin)
+	
+# Send a POST request to the login page with the form data
+response = session.post(url, data=fields)
+# print(response)
+# Check if the login was successful
+if response.status_code == 200:
+    # print("Logged In!")
+    # You can now access the protected pages using the same Session object
+    # The Session object will store the cookies and session data
+	# print(response.content)
+    # Example: Send a GET request to a protected page
+	response = session.get(url_account)
+	content_file = open("./response/content_response.txt", "w")
+	content_file.write(response.content.decode())
+	content_file.close()
+    # print(response.content)
+
+	with open('./response/content_response.txt') as f:
+        #for lines in f:
+		soup = BeautifulSoup(f, 'html.parser')
+		if (soup.find('span', {'id': 'credits_remaining'})):
+			form = soup.find('span', {'id': 'credits_remaining'}).string
+		else:
+			print(bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] The data entered does not correspond to any account\n" + bcolors.ENDC)
+			print(bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] Please, create an account first at https://www.spoofcard.com\n" + bcolors.ENDC)
 			sys.exit(0)
 
-os.remove("content_response.txt")
+        # Extract the form fields
+		fields = {}
+		# for input_field in form.find_all('h4'):
+		# print('Credits Remaining: ')
+		# print(form)
+		total_creds = form
 
-r = session.post(url_account, data=login, verify=False)
-
-content_file = open("content_response.txt", "w")
-content_file.write(r.content)
-content_file.close()
-
-with open('content_response.txt') as f:
-        for lines in f:
-                if re.search("credits_remaining", lines):
-			total_credits = lines.split('>')[2].split('<')[0]
-
-
-os.remove("content_response.txt")
-
-r = session.post(url_settings, verify=False)
-
-content_file = open("settings_response.txt", "w")
-content_file.write(r.content)
-content_file.close()
-
-with open('settings_response.txt') as f:
-        for lines in f:
-                if re.search("first_name:", lines):
-                        first_name = lines.split(':')[1].split('\'')[1]
-		if re.search("last_name:", lines):
-			last_name = lines.split(':')[1].split('\'')[1]
-		if re.search("email:", lines):
-			email = lines.split(':')[1].split('\'')[1]
-
-os.remove("settings_response.txt")
-
-print "*****************************"
-print "Credits on Account: %s " % total_credits + '\n'
-print "First name: %s" % first_name
-print "Second name: %s" % last_name
-print "Email: %s" % email
-print "*****************************"
-
-if total_credits == "0":
-	print '\n' + bcolors.FAIL + "You have no credits available" + bcolors.ENDC + '\n'
+else:
+	print(bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] The data entered does not correspond to any account\n" + bcolors.ENDC)
+	print(bcolors.OKGREEN + "[" + bcolors.ENDC + bcolors.OKBLUE + "*" + bcolors.ENDC + bcolors.OKGREEN + "] Please, create an account first at https://www.spoofcard.com\n" + bcolors.ENDC)
+	os.remove("./response/content_response.txt")
 	sys.exit(0)
 
-print "\n     " + bcolors.WARNING + "[" + bcolors.ENDC + bcolors.HEADER + "MENU" + bcolors.ENDC + bcolors.WARNING + "]" + bcolors.ENDC
-print "-----------------"
-print "1. Call Spoofing"
-print "2. SMS Spoofing"
-print "0. Exit"
-print "-----------------"
+print("*****************************")
+print("Credits on Account: %s " % total_creds + '')
+print("*****************************")
 
-menu_option = raw_input("Choose option: ")
+if total_creds == "0":
+	print('\n' + bcolors.FAIL + "You have no credits available" + bcolors.ENDC + '\n')
+	sys.exit(0)
+
+print("\n     " + bcolors.WARNING + "[" + bcolors.ENDC + bcolors.HEADER + "MENU" + bcolors.ENDC + bcolors.WARNING + "]" + bcolors.ENDC)
+print("-----------------")
+print("1. Call Spoofing")
+print("2. SMS Spoofing")
+print("0. Exit")
+print("-----------------")
+
+menu_option = input("Choose option: ")
 
 if menu_option == "1":
 	call_spoofing()
@@ -201,4 +227,5 @@ elif menu_option == "2":
 elif menu_option == "0":
 	sys.exit(0)
 else:
-	print "\nInvalid Option\n"
+	print("\nInvalid Option\n")
+
